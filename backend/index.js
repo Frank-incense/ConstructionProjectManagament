12e1536a7aa974d3dcda12e8199f8469f7eb1050
Your GitHub Username <audreygride@gmail.com>
2025-04-24 14:38:42 +0300

    questions

--- backend/index.js
+++ backend/index.js
@@ -1,7 +1,11 @@
 const express = require('express')
 const app = express();
 app.use(express.json());
+const bodyParser = require('body-parser');
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
 const port = 3000;
+
 
 app.get('/', (req, res) => {
   res.send('Hello World!')
@@ -9,9 +13,17 @@ app.get('/', (req, res) => {
 app.post('/ussd/post', (req, res) => {
   return res.status(200).send('Hello Post')
 })
-
+let questions = [
+  "Are you wearing your full PPE?",
+  "Have you inspected your tools & equipment?",
+  "Are there any new hazards observed today? ",
+  "Are all safety barriers and signage in place? ", 
+  
+]
+let count = 0
 app.post('/ussd', (req, res) => {
-  // Read the variables sent via POST from our API
+  
+  
   const {
       sessionId,
       serviceCode,
@@ -24,22 +36,23 @@ app.post('/ussd', (req, res) => {
   if (text == '') {
       // This is the first request. Note how we start the response with CON
       response = `CON What would you like to check
-      1. My account
-      2. My phone number`;
+      1. Safety standup;
+      2. leave`
+      count = 0;
   } else if ( text == '1') {
       // Business logic for first level response
-      response = `CON Choose account information you want to view
-      1. Account number`;
-  } else if ( text == '2') {
-      // Business logic for first level response
-      // This is a terminal request. Note how we start the response with END
-      response = `END Your phone number is ${phoneNumber}`;
-  } else if ( text == '1*1') {
-      // This is a second level response where the user selected 1 in the first instance
-      const accountNumber = 'ACC100101';
-      // This is a terminal request. Note how we start the response with END
-      response = `END Your account number is ${accountNumber}`;
+      response = `CON${questions[count]}
+      1. yes
+      2. no`; 
+      count++
   }
+  // } else if ( text == '1*1') {
+  //     // This is a second level response where the user selected 1 in the first instance
+  //     const accountNumber = 'ACC100101';
+  //     // This is a terminal request. Note how we start the response with END
+  //     response = `END Your account number is ${accountNumber}`;
+  //     count++
+  // }
 
   // Send the response back to the API
   res.set('Content-Type: text/plain');
