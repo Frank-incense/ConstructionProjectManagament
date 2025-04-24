8bc6c18c570d0ffd515c01be5c418cad40043334
Dennis Sammy <dennissammy77@gmail.com>
2025-04-24 16:44:04 +0300

    index page changes

--- backend/index.js
+++ backend/index.js
@@ -25,8 +25,14 @@ let questions = [
   "Are all safety barriers and signage in place? ", 
   
 ]
-let count = 0 
-let responses ={}
+let count = 0;
+let feedback = {
+  "phoneNumber": "",
+  "sessionId":"",
+  "feedbacks": [
+
+  ]
+}
 app.post('/ussd', (req, res) => {
   // Read the variables sent via POST from our API
   console.log(req.body)
@@ -38,31 +44,51 @@ app.post('/ussd', (req, res) => {
   } = req.body;
 
   let response = '';
-
   if (text == '') {
       // This is the first request. Note how we start the response with CON
       response = `CON What would you like to check
       1. Safety standup;
       2. leave`
       count = 0; 
-  } else if ( text == '1') {
-      
+  }else if(text == '2'){
+    response = `END bye ${phoneNumber} `; 
+  }else if(count === 4){
+    answer=text;
+    const arr = answer.split('*');
+    arr.forEach((element,index) => {
+      feedback.feedbacks.push(
+        {
+          "id": index,
+          "question": questions[index],
+          "answer": element
+        }
+      )
+    });
+    feedback.phoneNumber = phoneNumber;
+    feedback.sessionId = sessionId;
+    console.log(feedback)
+    response = `END bye ${phoneNumber} `; 
+  }else if ( text == '1') {
       // Business logic for first level response
       response = `CON ${questions[count]}
       1. yes
       2. no`; 
       count++;
-  }else if(text == '2' && count === 0){
-    response = `END bye ${phoneNumber} `; 
-  }else if(text == '2'  && count > 0){
+  }else if ( text.split('*')[count] == '1') {      
+    // Business logic for first level response
     response = `CON ${questions[count]}
-      1. yes
-      2. no`; 
-      count++;
+    1. yes
+    2. no`; 
+    count++;
+  }else if ( text.split('*')[count] == '2') {      
+    // Business logic for first level response
+    response = `CON ${questions[count]}
+    1. yes
+    2. no`; 
+    count++;
   }else{
     response = `END bye ${phoneNumber} `; 
-  }
-
+  };
   // Send the response back to the API
   res.set('Content-Type: text/plain');
   res.send(response);
