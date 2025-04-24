5cc10aebdb5f28d989b57701365ff191a8805f9b
shirocodes <ishirapt@gmail.com>
2025-04-24 17:39:56 +0300

    Added header.

--- client-side/src/routes.jsx
+++ client-side/src/routes.jsx
@@ -1,20 +1,18 @@
 import App from "./App";
 import AddOrder from "./pages/AddOrder";
 import OrderDashboard from "./pages/Dashboard";
+import Safety from "./pages/Safety";
 
 const routes = [
-    {
-        path: "/",
-        element: <App />,
-    },
-    {
-        path: "/orderDashboard",
-        element: <OrderDashboard />,
-    },
-    {
-        path: "/addOrder",
-        element: <AddOrder />,
-    },
-]
+  {
+    path: "/",
+    element: <App />,  // App is now the layout
+    children: [
+      { path: "orderDashboard", element: <OrderDashboard /> },
+      { path: "addOrder", element: <AddOrder /> },
+      { path: "safety", element: <Safety /> },
+    ]
+  },
+];
 
 export default routes;
